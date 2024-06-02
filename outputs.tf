acf8e49af3261bbe2491dae6ad45c0b0c196556c
batyak <batyak@checkpoint.com>
2024-06-02 21:42:59 +0300

    minor fix

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "get_azure_subscription_id" {
-  value = "${data.azurerm_subscription.scanner.subscription_id}"
+  value = "${data.dome9_cloudaccount_azure.azure_data_source.subscription_id}"
 }
