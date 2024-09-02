42872b5cc1103c43a1e51d117129a03558ac49de
Dana <danamo@checkpoint.com>
2024-09-02 10:47:45 +0300

    remove account issues

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -24,8 +24,3 @@ output "missing_awp_private_network_regions" {
   description = "List of regions in which AWP has issue to create virtual private network (VPC)"
   value       = module.terraform-dome9-awp-azure[0].missing_awp_private_network_regions
 }
-
-output "account_issues" {
-  description = "Indicates if there are any issues with AWP in the account"
-  value       = module.terraform-dome9-awp-azure[0].account_issues
-}
