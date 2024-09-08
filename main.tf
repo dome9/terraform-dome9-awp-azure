5dd25ae8c34bd6b33bf5d01151cd1bf84813a195
Dana <danamo@checkpoint.com>
2024-09-08 14:30:53 +0300

    adding remove script

--- main.tf
+++ main.tf
@@ -374,6 +374,54 @@ resource "azurerm_role_assignment" "cloudguard_function_apps_scan_operator_assig
     time_sleep.wait_for_function_apps_scan_operator_role_creation
   ]
 }
+
+resource "null_resource" "delete_awp_keys" {
+  count = "${terraform.workspace == "destroy" ? 1 : 0}"
+
+  provisioner "local-exec" {
+    command = <<EOT
+      delete_awp_keys_from_all_awp_vaults(){
+        AzOutput=$(az_wrapper keyvault list --subscription "$AWP_SUBSCRIPTION_ID" --query "[?tags.$AWP_OBSOLETE_OWNER_TAG_KEY == '$AWP_OWNER_TAG_VALUE' || tags.$AWP_OWNER_TAG_KEY == '$AWP_OWNER_TAG_VALUE'].name" -o tsv)
+        AzRetVal=$?
+        if [ $AzRetVal -eq 0 ] && [ -n "$AzOutput" ]; then
+          for keyvault in $AzOutput; do
+            delete_awp_keys_from_vault "$keyvault"
+          done
+        fi
+      }
+
+      delete_awp_keys_from_vault() {
+        _vault_name="$1"
+        _awp_obsolete_owner_key_lower=$(echo "$AWP_OBSOLETE_OWNER_TAG_KEY" | tr '[:upper:]' '[:lower:]')
+        _awp_owner_key_lower=$(echo "$AWP_OWNER_TAG_KEY" | tr '[:upper:]' '[:lower:]')
+
+        AzOutput=$(az_wrapper keyvault key list --subscription "$AWP_SUBSCRIPTION_ID" --vault-name "$_vault_name" --query "[?tags.$_awp_obsolete_owner_key_lower == '$AWP_OWNER_TAG_VALUE' || tags.$_awp_owner_key_lower == '$AWP_OWNER_TAG_VALUE'].name" -o tsv)
+        AzRetVal=$?
+
+        if [ $AzRetVal -eq 0 ] && [ -n "$AzOutput" ]; then
+          for key in $AzOutput; do
+            az_wrapper keyvault key delete --vault-name "$_vault_name" --name "$key"
+          done
+        fi
+      }
+
+      delete_awp_keys_from_all_awp_vaults
+    EOT
+
+    environment = {
+      AWP_SUBSCRIPTION_ID         = data.dome9_cloudaccount_azure.azure_data_source.subscription_id
+      AWP_OBSOLETE_OWNER_TAG_KEY  = local.AWP_OBSOLETE_OWNER_TAG_KEY
+      AWP_OWNER_TAG_KEY           = local.AWP_OWNER_TAG_KEY
+      AWP_OWNER_TAG_VALUE         = local.AWP_OWNER_TAG_VALUE
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = false
+  }
+}
+
+
 # END Assign custom roles based on scan mode
 
 
