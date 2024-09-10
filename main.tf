08857f8b29d34b03030e14557d35939cc9891034
Dana <danamo@checkpoint.com>
2024-09-10 08:51:01 +0300

    small change

--- main.tf
+++ main.tf
@@ -388,7 +388,7 @@ resource "null_resource" "delete_awp_keys" {
 
   provisioner "local-exec" {
     when    = destroy
-    command = "chmod +x ${path.module}/delete_keys.sh; ${path.module}/delete_keys.sh ${self.triggers.subscription_id} ${self.triggers.obsolete_owner_tag_key} ${self.triggers.owner_tag_key} ${self.triggers.owner_tag_value}"
+    command = "chmod +x ${path.module}/delete_awp_keys.sh; ${path.module}/delete_awp_keys.sh ${self.triggers.subscription_id} ${self.triggers.obsolete_owner_tag_key} ${self.triggers.owner_tag_key} ${self.triggers.owner_tag_value}"
   }
   lifecycle {
     create_before_destroy = false
